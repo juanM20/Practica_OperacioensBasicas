f2a5cd488fe2ed451329cd51808ecbdf06f34093
Juan Tellez <jmtp12poke@gmail.com>
2021-06-10 22:48:39 -0500

    Nuevos camibios

---
+++ Menu.m
@@ -0,0 +1,9 @@
+
+option = menu('Elige una categoría', 'Audio', 'Conjunto de muestras');
+
+switch option
+    case 1
+        GUI
+    case 2
+        GUI2
+end
---
+++ Practica_2.m
@@ -0,0 +1,31 @@
+[s,fs] = audioread('Prueba1.wav');
+
+tiempo = size(s,1)/fs;
+x = 0:1/fs:tiempo;
+num = 2;
+
+plot(x(2:end),s), xlim([0 tiempo]);
+
+l = s(:,1);
+r = s(:,2);
+
+sound(l,fs+25000); %Suma
+pause(4);
+sound(l+1,fs);
+pause(4);
+sound(l,fs-25000); %Resta
+pause(7);
+sound(l-1,fs);
+pause(4);
+sound(l*10,fs); %Amplificación
+pause(4);
+sound(l/5,fs); %Atenuación
+pause(4);
+sound(l(end:-1:1),fs); %Inversión
+pause(4);
+sound(l(1:num:end)/2,fs); %Diezmación
+pause(4);
+sound(); %Convolución
+pause(4);
+
+
